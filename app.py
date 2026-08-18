4ee8393edd52c6f40a40e276e823852d156c7ccc
marufbro360 <marufbro360@gmail.com>
2026-08-18 08:37:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ executor = ThreadPoolExecutor(max_workers=10)
 session = requests.Session()
 
 # --- Configuration ---
-API_KEY = "Maruf"
+API_KEY = "MARUF"
 BACKGROUND_FILENAME = "outfit.png"
 IMAGE_TIMEOUT = 8
 CANVAS_SIZE = (800, 800)
